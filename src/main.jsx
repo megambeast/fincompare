c5777e0e2739e8be18dc50d854e75ce3c5ae6d72
megambeast <michaellieu8@gmail.com>
2025-04-19 17:28:31 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import FinanceComparisonSite from './FinanceComparisonSite.jsx';
-import './index.css'; // optional if you want styling later
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
