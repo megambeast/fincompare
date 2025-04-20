fb1b6a028f5db44dff9735b252055b291fdedfd6
megambeast <michaellieu8@gmail.com>
2025-04-20 12:51:18 +1000

    Update SavingsAccountAustralia.jsx

--- src/SavingsAccountAustralia.jsx
+++ src/SavingsAccountAustralia.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import SavingsAccountList from './components/SavingsAccountList';
-import FinderScoreExplanation from './components/FinderScoreExplanation';
 import FAQSection from './components/FAQSection';
 import Footer from './components/Footer';
 
